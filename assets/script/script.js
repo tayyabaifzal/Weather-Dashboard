81544f2deb45ed6d5da45c329ce19802ee1b0264
Tayyaba <tayyaba.afzal@gmail.com>
2024-02-29 12:43:26 +0000

    js file modified. api key linked. elements linked using queryselector

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+let APIKey = "42747e74a920b7df0770c3dfae22459d";
+
+//Queryselecting existing elements 
+let searchBtn = $("#search-button");
+let cityInput = $("#search-input");
+let sideBar = $("#search-aside");
+let todayWeather = $("#today");
