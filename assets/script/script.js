9d04be72e91ac09a1fb35084f2db7895acf945f8
Tayyaba <tayyaba.afzal@gmail.com>
2024-02-29 16:01:00 +0000

    js code added to display weather,and get data from weather app

--- assets/script/script.js
+++ assets/script/script.js
@@ -53,15 +53,77 @@ let forecastday5 = moment().add(5, "days").format("D/MM/YYYY");
 // button for location history search
 $(document).ready(getHistory());
 
-function getHistory(){
+function getHistory() {
     buttonList.empty();
 
     historyArr = JSON.parse(localStorage.getItem("searchLocation"));
     console.log(historyArr);
-    
+
     console.log("locations array here: " + locationsArr);
     if (historyArr !== null) {
         locationsArr = historyArr;
         makeHistoryBtn();
     }
 };
+
+function makeHistoryBtn() {
+    buttonList.empty();
+    console.log("history array: " + historyArr);
+    console.log("locations array: " + locationsArr);
+
+    locationsArr = historyArr;
+    console.log("locations array now: " + locationsArr);
+
+    if (locationsArr !== null) {
+        locationsArr.forEach(item => {
+            locationBtn = $("<button>");
+            locationBtn.click(prevSearch);
+            buttonList.append(locationBtn);
+            //add place name
+            locationBtn.text(item)
+            locationBtn.attr("role", "button");
+            locationBtn.addClass("location-button btn-primary")
+
+            buttonList.before(historyTitle);
+        })
+    };
+};
+
+
+//primary search event listener:
+searchBtn.on("click", runSearch);
+
+function runSearch(event) {
+    event.preventDefault();
+    citySearch = cityInput.val();
+    locationsArr.push(citySearch);
+    historyArr = locationsArr;
+    localStorage.setItem("searchLocation", JSON.stringify(locationsArr));
+    
+    geoCode(); makeHistoryBtn();
+}
+
+
+//function to display weather from location button
+function prevSearch(event) {
+    citySearch = $(this).text();
+    cityInput.val(citySearch);
+    geoCode();
+};
+
+//Geocoding function
+function geoCode() {
+    cityInput.value = "";
+    let cityCoords = "https://api.openweathermap.org/geo/1.0/direct?q="+citySearch+"&appid=42747e74a920b7df0770c3dfae22459d"
+    $.ajax({
+        url: cityCoords,
+        method: "GET"
+    }).then(function(response) {
+    lat = (response[0].lat);
+    long = (response[0].lon);
+    getWeather();
+    });
+};
+
+
+=
