200b3e13cd42250ba455c4d39943a039531b3452
Tayyaba <tayyaba.afzal@gmail.com>
2024-02-29 12:44:39 +0000

    global elements added in the js file

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,3 +5,18 @@ let searchBtn = $("#search-button");
 let cityInput = $("#search-input");
 let sideBar = $("#search-aside");
 let todayWeather = $("#today");
+
+//initialising global variables
+let citySearch;
+var lat;
+var long;
+let responseGrab; //saved response
+let day; //for card number
+let cardTimes; //to get weather from same interval on each card
+var buttonList = $("#history");
+let historyTitle = $("<h4>");
+historyTitle.attr("id", "recent-header")
+historyTitle.html('<i class="fa-solid fa-clock-rotate-left"></i>  Recent searches:');
+var locationBtn;
+let locationsArr = [];
+let historyArr = [];
