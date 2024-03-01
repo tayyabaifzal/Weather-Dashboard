78d1cf024d939f2bffb2977ef5f16dc3c816d887
Tayyaba <tayyaba.afzal@gmail.com>
2024-03-01 19:42:21 +0000

    bugs fixed related to clear history button. css styling added along with comments.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,10 @@
-let APIKey = "42747e74a920b7df0770c3dfae22459d";
-
 //Queryselecting existing elements 
 let searchBtn = $("#search-button");
-let cityInput = $("#search-input");
+let searchInput = $("#search-input");
 let sideBar = $("#search-aside");
 let todayWeather = $("#today");
 
-//initialising global variables
+//initialising variables
 let citySearch;
 var lat;
 var long;
@@ -17,7 +15,7 @@ var buttonList = $("#history");
 let historyTitle = $("<h4>");
 historyTitle.attr("id", "recent-header")
 historyTitle.html('<i class="fa-solid fa-clock-rotate-left"></i>  Recent searches:');
-var locationBtn;
+var locationButton;
 let locationsArr = [];
 let historyArr = [];
 
@@ -31,89 +29,103 @@ let humidity;
 let todayIconCode;
 
 //forecast cards
-let tempKelvCard;
-let celsiusCard = (tempKelvCard - 273.15).toFixed(2);
-let windCard;
-let humidityCard;
-let iconCard;
+let tempKelvFC;
+let celsiusFC = (tempKelvFC - 273.15).toFixed(2);
+let windFC;
+let humidityFC;
+let iconFC;
 
-//5-day forecast cards
+//5-day forecast cards:
 let forecast = $("#forecast");
 let fiveDayTitle = $("<h3>").addClass("fiveDayTitle");
 fiveDayTitle.text("5-Day Forecast:")
 
-//moment.js for dates
+//moment.js for dates:
 let today = moment().format("D/MM/YYYY");
-let forecastday1 = moment().add(1, "days").format("D/MM/YYYY");
-let forecastday2 = moment().add(2, "days").format("D/MM/YYYY");
-let forecastday3 = moment().add(3, "days").format("D/MM/YYYY");
-let forecastday4 = moment().add(4, "days").format("D/MM/YYYY");
-let forecastday5 = moment().add(5, "days").format("D/MM/YYYY");
+let day1 = moment().add(1, "days").format("D/MM/YYYY");
+let day2 = moment().add(2, "days").format("D/MM/YYYY");
+let day3 = moment().add(3, "days").format("D/MM/YYYY");
+let day4 = moment().add(4, "days").format("D/MM/YYYY");
+let day5 = moment().add(5, "days").format("D/MM/YYYY");
+
 
 // button for location history search
 $(document).ready(getHistory());
 
-function getHistory() {
+
+function getHistory(){
     buttonList.empty();
 
     historyArr = JSON.parse(localStorage.getItem("searchLocation"));
     console.log(historyArr);
-
+    
     console.log("locations array here: " + locationsArr);
     if (historyArr !== null) {
         locationsArr = historyArr;
-        makeHistoryBtn();
+        makeHistoryButton();
     }
 };
 
-function makeHistoryBtn() {
+
+
+function makeHistoryButton() {
     buttonList.empty();
     console.log("history array: " + historyArr);
     console.log("locations array: " + locationsArr);
 
+    
     locationsArr = historyArr;
     console.log("locations array now: " + locationsArr);
-
+    // historyArr = locationsArr;
     if (locationsArr !== null) {
-        locationsArr.forEach(item => {
-            locationBtn = $("<button>");
-            locationBtn.click(prevSearch);
-            buttonList.append(locationBtn);
-            //add place name
-            locationBtn.text(item)
-            locationBtn.attr("role", "button");
-            locationBtn.addClass("location-button btn-primary")
+    locationsArr.forEach(item => {
+    //create button
+    
+    locationButton = $("<button>");
+    locationButton.click(prevSearch);
+    buttonList.append(locationButton);
+    //add place name
+    locationButton.text(item)
+    locationButton.attr("role", "button");
+    locationButton.addClass("location-button btn-primary")
 
-            buttonList.before(historyTitle);
-        })
-    };
+    buttonList.before(historyTitle);
+    })};
 };
 
 
 //primary search event listener:
 searchBtn.on("click", runSearch);
 
+
 function runSearch(event) {
     event.preventDefault();
-    citySearch = cityInput.val();
+    citySearch = searchInput.val();
+    // const storageTest = localStorage.getItem("searchLocation");
+    // if (storageTest !== null) {
+        // locationsArr = JSON.parse(localStorage.getItem("searchLocation"));
+    // };
     locationsArr.push(citySearch);
     historyArr = locationsArr;
     localStorage.setItem("searchLocation", JSON.stringify(locationsArr));
     
-    geoCode(); makeHistoryBtn();
+    geoCode(); makeHistoryButton();
 }
+
 
 
 //function to display weather from location button
 function prevSearch(event) {
+    // event.preventDefault();
     citySearch = $(this).text();
-    cityInput.val(citySearch);
+    searchInput.val(citySearch);
     geoCode();
 };
 
+
 //Geocoding function
 function geoCode() {
-    cityInput.value = "";
+    searchInput.value = "";
     let cityCoords = "https://api.openweathermap.org/geo/1.0/direct?q="+citySearch+"&appid=42747e74a920b7df0770c3dfae22459d"
     $.ajax({
         url: cityCoords,
@@ -126,8 +138,10 @@ function geoCode() {
 };
 
 
+
 //get Weather function
 function getWeather(){
+    // searchInput.val("");
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=42747e74a920b7df0770c3dfae22459d&units=metric";
     $.ajax({
         url: queryURL,
@@ -146,6 +160,9 @@ function getWeather(){
     showToday();
     });
 };
+
+
+
 
 //show Weather function
 function showToday() {
@@ -178,6 +195,7 @@ function showToday() {
 }
 
 
+
 //makes cards for 5-day forecast
 function makeCards(){ 
     todayWeather.after(fiveDayTitle);
@@ -193,6 +211,7 @@ function makeCards(){
     // give card ID
         forecastCard.attr("id", "day"+(day))
 
+
 // designing cards:
     let cardTitle = $("<h5>").addClass("card-title");
     let cardIcon = $("<img>");
@@ -200,20 +219,22 @@ function makeCards(){
     let cardWind =  $("<p>").addClass("card-text");
     let cardHumidity =  $("<p>").addClass("card-text");
 
-// grab info for cards
-    celsiusCard = (responseGrab.list[cardTimes[i]].main.temp).toFixed(1);
-    windCard = responseGrab.list[cardTimes[i]].wind.speed;
-    humidityCard =  responseGrab.list[cardTimes[i]].main.humidity;
-    iconCard = responseGrab.list[cardTimes[i]].weather[0].icon;
-    let iconURL = "https://openweathermap.org/img/wn/" + iconCard + "@2x.png";
 
-// setting card content
+// grab info for cards
+    celsiusFC = (responseGrab.list[cardTimes[i]].main.temp).toFixed(1);
+    windFC = responseGrab.list[cardTimes[i]].wind.speed;
+    humidityFC =  responseGrab.list[cardTimes[i]].main.humidity;
+    iconFC = responseGrab.list[cardTimes[i]].weather[0].icon;
+    let iconURL = "https://openweathermap.org/img/wn/" + iconFC + "@2x.png";
+
+
+// setting card content:
     cardTitle.text(dayX);
     cardIcon.attr("src", iconURL);
-    cardTemp.text("Temp: " + celsiusCard + "°c");
-    cardWind.text("Wind: " + windCard + "KPH");
-    cardHumidity.text("Humidity: " + humidityCard + "%");
-// appending cards
+    cardTemp.text("Temp: " + celsiusFC + "°c");
+    cardWind.text("Wind: " + windFC + "KPH");
+    cardHumidity.text("Humidity: " + humidityFC + "%");
+// appending cards:
     forecastCard.append(cardTitle);
     forecastCard.append(cardIcon);
     forecastCard.append(cardTemp);
@@ -226,7 +247,7 @@ function makeCards(){
 };
 
 
-//clear button
+//clear button:
 let clearBtn = $("<button>");
 clearBtn.attr("id", "clear-button");
 clearBtn.html('<i class="fa-regular fa-trash-can"></i> Clear history');
